efd0bbdf050f6e732da17af27a37320017fe430c
enkiv2 <john.ohno@gmail.com>
2014-10-31 11:40:53 -0400

    add title generator

--- herosjourney.sh
+++ herosjourney.sh
@@ -2,7 +2,9 @@
 
 HERO="$(cat firstNames.txt | sort -R | head -n 1) $(cat lastNames.txt | sort -R | head -n 1)"
 HOMETOWN="$(cat towns.txt | sort -R | head -n 1)"
+OTHERWORLD="$(cat imaginaryPlaces.txt | sort -R | head -n 1)"
 SENEX="$(echo -e "$(echo -e {Old\ ,Old\ Man\ ,Sir\ ,Dr.\ ,Professor\ ,Master\ ,Brother\ ,}\\n | sort -R | head -n 1)$(echo -e {$(cat firstNames.txt | sort -R | head -n 1)\ ,}\\n | sort -R | head -n 1)$(cat lastNames.txt | sort -R | head -n 1)$(echo -e {\ the\ $(echo -e {Grey,White,Red,Brown,Green,Violet,Gold,Silver,Traveller,Mad\ Arab,Mad,Ancient,Wise,Necromancer,Mage,Magician,Wizard,Monk,Paladin,Thief,Shaman,Rogue,Knight,Warrior,Priest,Alchemist,Elf,Dwarf,Orc,Man,Night\ Elf,Halfling,Fisher\ King,Hangman,Dead,Dread,Dragonborn,Yeti\ Tamer,Dog\ Whisperer}\\n | sort -R | head -n 1),}\\n | sort -R | head -n 1)$(echo -e {\ of\ $(cat towns.txt | sort -R | head -n 1 | sed 's/ /\\ /g'),}\\n | sort -R | head -n 1)" | sort -R | head -n 1 | sed 's/,}//g' | tr -d '{}\\' | sed 's/  */ /g')"
+TITLE="$(echo -e $HERO{,\ and\ $SENEX}\ in\ $OTHERWORLD\\nFrom\ $HOMETOWN\ to\ $OTHERWORLD{,\ and\ Back\ Again}\\nThe\ {Secret,Mystery}\ of\ {$OTHERWORLD,$HOMETOWN} | sort -R | head -n 1)"
 
 SEPARATION="$CallToAdventure\n$RefusalOfTheCall\n$SupernaturalAid\n$CrossingTheThreshhold\n$BellyOfTheWhale\n"
 INITIATION="$TheRoadOfTrials\n$TheMeetingWithTheGoddess\n$WomanAsTemptress\n$AtonementWithTheFather\n$Apotheosis\n$TheUltimateBoon\n"
@@ -12,3 +14,5 @@ NARRATIVE="$SEPARATION\n$INITIATION\n$RETURN\n"
 echo "Hero: $HERO"
 echo "Hometown: $HOMETOWN"
 echo "Senex: $SENEX"
+echo "Other world: $OTHERWORLD"
+echo "Title: $TITLE"
